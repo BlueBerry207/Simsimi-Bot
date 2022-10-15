36a83b736d31fbde2deba9379d986f1d88fb60c1
BlueBerryy123 <103749583+BlueBerryy123@users.noreply.github.com>
2022-10-16 02:13:31 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const TOKEN = 'MTAyNjA3OTE2MzI5MzExMDI4Mg.GOu9Ps.y2sIGGWgz-5ES_GzzPRxHN2Vhyou1-tvbbxT00'
-export const DB = 'mongodb+srv://TripleJ:Kjh080707@cluster0.ymbte.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+export const TOKEN = 'Bot Token'
+export const DB = 'Mongoose data base url'
